b87e80d407a566f869207f7f9e3b38ba3db6ced3
haozhi <wisdomh226@163.com>
2019-01-21 20:59:15 +0800

    c++ class stock

---
+++ stock10.h
@@ -0,0 +1,25 @@
+// stock10.h -- Stock class declaration with construction, destructor added
+#ifndef STOCK10_H_
+#define STOCK10_H_
+#include <string>
+
+class Stock
+{
+    private:
+        std::string company;
+        long shares;
+        double share_val;
+        double total_val;
+        void set_tot() { total_val = shares * share_val; };
+    public:
+        // two constructions
+        Stock(); // default constructor
+        Stock(const std::string & co, long n = 0, double pr = 0.0);
+        ~Stock(); // noisy constructor
+        void buy(long num, double price);
+        void sell(long num, double price);
+        void update(double price);
+        void show();
+}; // notice the semicolon
+
+#endif
